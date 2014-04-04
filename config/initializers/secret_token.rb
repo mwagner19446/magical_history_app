ee0337c26b5e9e4c4aa68856175631e26b99cea7
Michael K. Wagner <michael.k.wagner@gmail.com>
2014-04-04 17:10:16 -0400

    Update Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MagicalHistoryApp::Application.config.secret_key_base = '72c346ab9458ad596dbff82cee7b1a3ffb89e6b85a35e3d61b8fd9d560e6b449489c3259c93ddc3d7688946c6acff5b063488454fd9ccb2ff2888a06956c9fde'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MagicalHistoryApp::Application.config.session_store :cookie_store, key: '_magical_history_app_session'
