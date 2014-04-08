b79f57e486c65140bf684e539d89b7d936f15e70
Michael K. Wagner <michael.k.wagner@gmail.com>
2014-04-08 13:23:58 -0400

    Update Tour Screen to adjust for Waypoints

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-
+//= require jquery.ui.all
 
 //= require turbolinks
 //= require_tree .
-//= require underscore
-//= require backbone
--- app/assets/javascripts/googlemaps.js
+++ app/assets/javascripts/googlemaps.js
@@ -1,3 +1,6 @@
+$(".waypoints-header").tooltip(); 
+
+
 var directionsDisplay;
 var directionsService = new google.maps.DirectionsService();
 var request = {}
@@ -20,38 +23,61 @@ function initialize(){
 }   
 
 //Moving Away from this: 
-function getWaypoints(){
-  x = []
-  locations = $("input:checked").parent().text()
-  locationsArray = locations.split("|")
-  for (var i= 0; i<locationsArray.length; i++ ){
-    x.push({
-      location: locationsArray[i]
-    });
-  }
-  // locationsArray.forEach(function(z){
-  //   x.push({location: z})
-  // });
-  debugger;   
-  return x
-}
+// function getWaypoints(){
+//   x = []
+//   locations = $("input:checked").parent().text()
+//   locationsArray = locations.split("|")
+//   for (var i= 0; i<locationsArray.length; i++ ){
+//     x.push({
+//       location: locationsArray[i]
+//     });
+//   }
+//   // locationsArray.forEach(function(z){
+//   //   x.push({location: z})
+//   // });
+//   return x
+// }
 
 function calcRoute() {
-  var lon = parseFloat($("#tour-longitude").html())
-  var lat = parseFloat($("#tour-latitude").html())
-  var myLatLng = new google.maps.LatLng(lat,lon)
-  var waypts = []
+  lon = parseFloat($("#tour-longitude").html())
+  lat = parseFloat($("#tour-latitude").html())
+  myLatLng = new google.maps.LatLng(lat,lon)
+  waypts = []
 
-  var locations = $("input:checked").parent().text()
-  var locationsArray = locations.split("|")
-  locationsArray.forEach(function(z){
-    waypts.push({location: z})
-  });  
+  // Step 1:  Capture the LongLat
+  // Step 2:  Add the Google Longlat Object to the Waypoints hash. 
 
-  var request = {
+  locations = $("input:checked").parent().text()
+  if(locations != ""){
+    locationsArray = locations.split("|")  
+    newLocationsArray = $.map(locationsArray, function(v){
+      return v.trim() === "" ? null : v.trim(); 
+    })
+
+    newLocationsArray.forEach(function(z){
+      xlon = z.match(/Longitude:(.*)/)
+      xlat = z.match(/Latitude:(.*)/)
+
+      zlon = parseFloat(xlon[1].trim())
+      zlat = parseFloat(xlat[1].trim())
+      
+      zloc = zlat +" , "+zlon
+      // parseFloat(xlon.trim())
+      waypts.push({location: zloc})
+      // waypts.push({location: zlat, zlon})
+      // waypts.push({location: z.trim()+ " NYC"})
+    });
+  }
+
+    
+    
+  request = {
     origin: $("input[name='start']").val() || myLatLng,
     destination: $("input[name='destination']").val() || myLatLng,
     waypoints: waypts, 
+
+
+
 
     // [
     //     {location: "Gracie Mansion"}, 
@@ -59,6 +85,12 @@ function calcRoute() {
     //     {location: "Neue Gallerie"}, 
     //     {location: "John Jay Park"}
     //   ], 
+
+    // [
+    //   {location: new google.maps.LatLng(40.76964,-73.961886)}, 
+    //   {location: new google.maps.LatLng(40.7813,-73.9603)}, 
+    // ],
+
 
     optimizeWaypoints: true,  
     travelMode: google.maps.TravelMode.WALKING 
